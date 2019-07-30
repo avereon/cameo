e0ad943df176c53cfc390c967e6784e72bf043a3
Mark Soderquist <mark@soderquist.net>
2019-07-29 19:41:37 -0600

    Save images to downloads folder

--- source/main/java/com/avereon/cameo/Main.java
+++ source/main/java/com/avereon/cameo/Main.java
@@ -3,14 +3,16 @@ package com.avereon.cameo;
 import com.avereon.xenon.icon.WingDiscLargeIcon;
 import com.avereon.xenon.icon.XRingLargeIcon;
 
-import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Main {
 
 	public static void main( String[] commands ) {
+		Path downloads = Paths.get( System.getProperty( "user.home" ), "Downloads" );
 		try {
-			ProgramImageWriter.save( new WingDiscLargeIcon(), new File( "avereon.png" ) );
-			ProgramImageWriter.save( new XRingLargeIcon(), new File( "xenon.png" ) );
+			ProgramImageWriter.save( new WingDiscLargeIcon(), downloads.resolve( "avereon.png" ).toFile() );
+			ProgramImageWriter.save( new XRingLargeIcon(), downloads.resolve( "xenon.png" ).toFile() );
 			ProgramImageWriter.done();
 		} catch( Throwable throwable ) {
 			throwable.printStackTrace( System.err );
