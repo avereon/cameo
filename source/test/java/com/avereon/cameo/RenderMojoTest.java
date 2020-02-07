51bf80471209b8bdf35b902733587ca9d5248d32
Mark Soderquist <soderquistmv@churchofjesuschrist.org>
2020-02-07 11:36:53 -0700

    Add more unit tests

--- source/test/java/com/avereon/cameo/RenderMojoTest.java
+++ source/test/java/com/avereon/cameo/RenderMojoTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -33,15 +35,50 @@ public class RenderMojoTest {
 	}
 
 	@Test
-	void testExecuteWithImage() throws Exception {
+	void testPngImage() throws Exception {
 		ImageMetadata imageMetadata = new ImageMetadata();
 		imageMetadata.setClass( BrokenIcon.class.getName() );
+		imageMetadata.setSize( 256 );
 		imageMetadata.setTarget( "target/images/broken.png" );
 		mojo.setImages( new ImageMetadata[]{ imageMetadata } );
 
 		mojo.execute();
 
 		assertTrue( new File( "target/images/broken.png" ).exists() );
+	}
+
+	@Test
+	void testPngIcon() throws Exception {
+		ImageMetadata imageMetadata = new ImageMetadata();
+		imageMetadata.setClass( BrokenIcon.class.getName() );
+		imageMetadata.setSize( 64 );
+		IconMetadata iconMetadata = new IconMetadata();
+		iconMetadata.setImages( new ImageMetadata[]{ imageMetadata } );
+		iconMetadata.setTarget( "target/icons/broken.png" );
+		mojo.setIcons( new IconMetadata[]{ iconMetadata } );
+
+		mojo.execute();
+
+		assertTrue( new File( "target/icons/broken.png" ).exists() );
+	}
+
+	@Test
+	void testIcoIcon() throws Exception {
+		List<ImageMetadata> icons = new ArrayList<>();
+		for( int exp = 8; exp >= 4; exp--) {
+			ImageMetadata imageMetadata = new ImageMetadata();
+			imageMetadata.setClass( BrokenIcon.class.getName() );
+			imageMetadata.setSize( (int)Math.pow( 2, exp) );
+			icons.add( imageMetadata );
+		}
+		IconMetadata iconMetadata = new IconMetadata();
+		iconMetadata.setImages( icons.toArray( new ImageMetadata[0] ) );
+		iconMetadata.setTarget( "target/icons/broken.ico" );
+		mojo.setIcons( new IconMetadata[]{ iconMetadata } );
+
+		mojo.execute();
+
+		assertTrue( new File( "target/icons/broken.ico" ).exists() );
 	}
 
 	@Test
