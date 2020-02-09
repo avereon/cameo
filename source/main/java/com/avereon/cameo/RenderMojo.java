f6610091a474c0c77531d951f059b2fe31992b88
Mark Soderquist <mark@soderquist.net>
2020-02-08 17:25:34 -0700

    Remove unused classpath logic

--- source/main/java/com/avereon/cameo/RenderMojo.java
+++ source/main/java/com/avereon/cameo/RenderMojo.java
@@ -2,30 +2,24 @@ package com.avereon.cameo;
 
 import com.avereon.venza.image.ProgramImage;
 import com.avereon.venza.image.ProgramImageWriter;
-import org.apache.maven.artifact.Artifact;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
-import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
 
 import java.io.File;
 import java.lang.reflect.Constructor;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @SuppressWarnings( "unused" )
-@Mojo( name = "render", defaultPhase = LifecyclePhase.PREPARE_PACKAGE, requiresDependencyCollection = ResolutionScope.COMPILE, requiresDependencyResolution = ResolutionScope.COMPILE )
+@Mojo( name = "render", defaultPhase = LifecyclePhase.PREPARE_PACKAGE )
 public class RenderMojo extends AbstractMojo {
 
 	@Parameter( readonly = true, defaultValue = "${project}" )
@@ -43,19 +37,9 @@ public class RenderMojo extends AbstractMojo {
 	public void execute() throws MojoExecutionException, MojoFailureException {
 		if( images == null && icons == null ) throw new MojoExecutionException( "No images or icons to render" );
 
-		// Sort the artifacts
-		Set<Artifact> artifacts = project.getArtifacts();
-		List<URL> urls = artifacts.stream().map( a -> {
-			try {
-				return a.getFile().toURI().toURL();
-			} catch( MalformedURLException e ) {
-				return null;
-			}
-		} ).filter( Objects::nonNull ).collect( Collectors.toList() );
-
 		try {
-			urls.add( 0, new File( project.getBuild().getOutputDirectory() ).toURI().toURL() );
-			urls.forEach( u -> getLog().debug( "url=" + u ) );
+			List<URL> urls = List.of( new File( project.getBuild().getOutputDirectory() ).toURI().toURL() );
+			urls.forEach( u -> getLog().info( "url=" + u ) );
 
 			// Create special class loader that includes the recently created classes
 			loader = new URLClassLoader( urls.toArray( new URL[ 0 ] ), getClass().getClassLoader() );
