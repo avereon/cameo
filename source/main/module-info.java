a2f2a3ac5980c3300a24e3abc55e331b8ee7435d
Mark Soderquist <mark@soderquist.net>
2025-09-12 20:36:00 -0600

    Update module-info.java to include additional static Maven plugin dependencies

--- source/main/module-info.java
+++ source/main/module-info.java
@@ -1,7 +1,12 @@
 module cameo {
+	requires static com.avereon.zenna;
+	requires static maven.plugin.api;
+	requires static maven.plugin.annotations;
+	requires static maven.project;
+
 	requires transitive javafx.controls;
 	requires transitive javafx.graphics;
-	requires static com.avereon.zenna;
+
 	requires com.avereon.zerra;
 
 	exports com.avereon.cameo;
